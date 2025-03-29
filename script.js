a7999b9b5bf6795a521a330d073cdac008a6a815
Thomas-Brito <thomasbrito.tjb@gmail.com>
2025-03-28 23:49:50 -0300

    Adicionando modo escuro

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-const html = document.documentElement;
+{
+  const caixa = document.getElementById("minha-caixa"); // Caixa interativa
+  const abre = document.getElementById("abrir-caixa"); // Botão de abrir
+  const fecha = document.getElementsByClassName("fechar-caixa")[0]; // Botão de fechar
 
-function mudarTema() {
-  const temaAtual = html.getAttribute("data-theme");
-  const novoTema = temaAtual === "dark" ? "light" : "dark";
-  html.setAttribute("data-theme", novoTema);
+  // Mostra a caixa interativa ao clicar no botão de abrir
+  abre.onclick = function () { 
+    caixa.style.display = "block";
+  }
+
+  // Para de mostrar a caixa interativa ao clicar no botão de fechar
+  fecha.onclick = function () {
+    caixa.style.display = "none";
+  }
 }
 
-document.getElementById("alternar").addEventListener("click", mudarTema);
+{
+  // Função que muda o tema de acordo com o evento de mudança de seleção de botão radio, com base no value='' do name='tema'
+  function mudarTema(event) {
+    const temaAtual = event.target.value;
+    document.documentElement.setAttribute("data-theme", temaAtual);
+  }
+
+  const botoesTema = document.querySelectorAll('input[name="tema"]');
+  botoesTema.forEach((botao) => {
+    botao.addEventListener("change", mudarTema);
+  })
+}
